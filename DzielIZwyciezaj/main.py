eadf69c2eddb95478c3ff3fbbc8b8d994f488da2
filip k <filip2006kujawa@gmail.com>
2024-02-07 10:05:47 +0100

    koniec lekcji

--- DzielIZwyciezaj/main.py
+++ DzielIZwyciezaj/main.py
@@ -1,32 +1,43 @@
 # f(x) = x^2 - 2
 
 
-def f1(x):
-    result = x**2 - 2
-    return result
-
-
-f2 = lambda x: x**2 - 2
-
-
-left, right = 0, 2
-
-
 def bisect(f, left, right):
 
-    value_left = f(left)
-    value_right = f(right)
+    while True:
+        value_left = f(left)
+        value_right = f(right)
 
-    if value_left * value_right > 0:
-        raise ValueError("liczby są tych samych znaków")
+        if value_left * value_right > 0:
+            raise ValueError("liczby są tych samych znaków")
+
+        center = (left + right) / 2
+
+        value_center = f(center)
+
+        # print("\n\n")
+        # print("left->", left, value_left)
+        # print("right->", right, value_right)
+        # print("center->", center, value_center)
+
+        if value_left * value_center < 0:
+            left = left
+            right = center
+        else:
+            left = center
+            right = right
+
+        if right - left < 1e-14:
+            return center
 
 
-def f1(x):
-    result = x**2 - 2
-    return result
+import math
+
+a = 3
+b = 4
+
+f2 = lambda c: math.sqrt(a**2 + b**2) - c
 
 
-f2 = lambda x: x**2 - 2
+result = bisect(f2, 3, 6)
 
-
-bisect(f2, 2, 4)
+print(result)
